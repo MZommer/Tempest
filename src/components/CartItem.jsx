7ac84bf89dc343d0f1ecf86d24e44a19c9dfe1b6
yaetesler <yaetesler@gmail.com>
2022-10-31 11:27:18 -0300

    with qr button

--- src/components/CartItem.jsx
+++ src/components/CartItem.jsx
@@ -3,7 +3,7 @@ import { formatPrice } from '@utils/helpers';
 import AmountButtons from './AmountButtons';
 import { FaTrash } from 'react-icons/fa';
 import { useCartContext } from '@contexts/CartContext';
-const CartItem = ({ id, image, name, color, price, amount }) => {
+const CartItem = ({ id, Thumb, Name, Price, amount }) => {
   const { removeItem, toggleAmount } = useCartContext();
   const increase = () => {
     toggleAmount(id, 'inc');
@@ -16,18 +16,15 @@ const CartItem = ({ id, image, name, color, price, amount }) => {
   return (
     <Wrapper>
       <div className='title'>
-        <img src={image} alt={name} />
+        <img src={Thumb} alt={Name} />
         <div>
-          <h5 className='name'>{name}</h5>
-          <p className='color'>
-            color : <span style={{ background: color }}></span>
-          </p>
-          <h5 className='price-small'>{formatPrice(price)}</h5>
+          <h5 className='name'>{Name}</h5>
+          <h5 className='price-small'>{formatPrice(Price)}</h5>
         </div>
       </div>
-      <h5 className='price'>{formatPrice(price)}</h5>
+      <h5 className='price'>{formatPrice(Price)}</h5>
       <AmountButtons amount={amount} increase={increase} decrease={decrease} />
-      <h5 className='subtotal'>{formatPrice(price * amount)}</h5>
+      <h5 className='subtotal'>{formatPrice(Price * amount)}</h5>
       <button type='button' className='remove-btn' onClick={() => removeItem(id)}>
         <FaTrash />
       </button>

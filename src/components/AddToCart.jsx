91542506029e389f8810f2b04036fe2e3dedcff3
Matias Zommer <87200156+MZommer@users.noreply.github.com>
2022-11-10 00:07:31 -0300

    Fixed cart

--- src/components/AddToCart.jsx
+++ src/components/AddToCart.jsx
@@ -6,7 +6,7 @@ import AmountButtons from './AmountButtons';
 
 const AddToCart = ({ product }) => {
   const { addToCart } = useCartContext();
-  const { id, stock } = product;
+  const { ID, stock } = product;
   const [amount, setAmount] = useState(1);
 
   const increase = () => {
@@ -33,7 +33,7 @@ const AddToCart = ({ product }) => {
     <Wrapper>
       <div className='btn-container'>
         <AmountButtons amount={amount} increase={increase} ServingSize={product.NutritionalTable.ServingSize} decrease={decrease} />
-        <Link to='/cart' className='btn' onClick={() => addToCart(id, amount, product)}>
+        <Link to='/cart' className='btn' onClick={() => addToCart(ID, amount, product)}>
           Agregar al carrito
         </Link>
       </div>
